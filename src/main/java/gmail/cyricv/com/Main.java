56281d7f86c0680e3312d343cfed3fcc92694c8c
Paden Atkinson <CyricV@Gmail.com>
2015-03-22 13:30:07 -0700

    forge/eclipse setup

---
+++ src/main/java/gmail/cyricv/com/Main.java
@@ -0,0 +1,30 @@
+package gmail.cyricv.com;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="", version="")
+public class Main {
+	public static String MODID = "cyricv_tut";
+	public static String VERSION = "0.0.0";
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e)
+	{
+		
+	}
+	@EventHandler
+	public void init(FMLInitializationEvent e)
+	{
+		
+	}
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e)
+	{
+		
+	}
+	
+}
